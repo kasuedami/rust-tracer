54472db66206c023d77f3acb2c0d4a6605faf9be
Sebastian Nocke <sjno@gmx.de>
2023-09-11 16:32:20 +0200

    Reworked image writer

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,57 @@
+const COLOR_RESOLUTION: u32 = 255;
+
 pub mod image_writer {
+    use super::COLOR_RESOLUTION;
+
     use std::{path::Path, fs::File, io::{Write, Error}};
 
-    use indicatif::ProgressBar;
+    use glam::DVec3;
+    use indicatif::ProgressIterator;
+    use itertools::Itertools;
 
     pub fn write(width: u32, height: u32, path: &Path) -> Result<(), Error> {
 
-        let mut ppm_content = String::new();
-        ppm_content.push_str(&format!("P3\n{} {}\n255", width, height));
+        let pixels = (0..height)
+            .cartesian_product(0..width)
+            .progress_count(width as u64 * height as u64)
+            .map(|(y, x)| {
 
-        let bar = ProgressBar::new(width as u64 * height as u64);
-
-        for j in 0..height {
-            for i in 0..width {
-                let r = i as f32 / (width - 1) as f32;
-                let g = j as f32 / (height - 1) as f32;
-                let b = 0.0;
-
-                let int_r = (255.999 * r) as i32;
-                let int_g = (255.999 * g) as i32;
-                let int_b = (255.999 * b) as i32;
-
-                ppm_content.push_str(&format!("{} {} {}", int_r, int_g, int_b));
-                bar.inc(1);
-            }
-        }
-
-        bar.finish();
+                let color = DVec3::new(
+                    x as f64 / (width - 1) as f64,
+                    y as f64 / (height - 1) as f64,
+                    0.0)
+                    .clamp(
+                        DVec3::splat(0.0),
+                        DVec3::splat(0.999)
+                    ) * 256.0;
+                
+                format!(
+                    "{} {} {}",
+                    color.x, color.y, color.z
+                )
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
 
         if let Some(parent) = path.parent() {
             std::fs::create_dir_all(parent)?;
         }
 
         let mut file = File::create(path)?;
-        write!(file, "{}", ppm_content)?;
+        write!(
+            file,
+            "{}",
+            format!(
+                "P3
+{} {}
+{}
+{pixels}
+",
+                width,
+                height,
+                COLOR_RESOLUTION
+            )
+        )?;
 
         Ok(())
     }
