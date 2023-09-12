0fb6105d74687daa13fc13216ac24ed963d7f5bb
Sebastian Nocke <sjno@gmx.de>
2023-09-12 18:53:27 +0200

    Added fuzz

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() {
 
     let material_ground = Rc::new(Lambertian::new(DVec3::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(DVec3::new(0.7, 0.3, 0.3)));
-    let material_left = Rc::new(Metal::new(DVec3::new(0.8, 0.8, 0.8)));
-    let material_right = Rc::new(Metal::new(DVec3::new(0.8, 0.6, 0.2)));
+    let material_left = Rc::new(Metal::new(DVec3::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Rc::new(Metal::new(DVec3::new(0.8, 0.6, 0.2), 1.0));
 
     let objects: Vec<Box<dyn Hittable>> = vec![
         Box::new(Sphere::new(DVec3::new(0.0, -100.5, -1.0), 100.0, material_ground)),
@@ -20,7 +20,7 @@ fn main() {
     let world = World::new(objects);
 
     let position = DVec3::new(0.0, 0.0, 0.0);
-    let image = Image::from_width_aspect_ratio(400, 2.0, 255);
+    let image = Image::from_width_aspect_ratio(400, 16.0 / 9.0, 255);
     let mut camera = Camera::new(position, 1.0, 50, 10, image);
 
     camera.render_image(&world);
--- src/material/metal.rs
+++ src/material/metal.rs
@@ -2,24 +2,36 @@ use glam::DVec3;
 
 use crate::{ray::Ray, hittable::HitRecord};
 
-use super::{Material, Scattered, reflect::reflect};
+use super::{Material, Scattered, reflect::reflect, util::random_unit_vector};
 
 pub struct Metal {
     albedo: DVec3,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: DVec3) -> Self {
-        Self { albedo }
+    pub fn new(albedo: DVec3, fuzz: f64) -> Self {
+
+        let fuzz = if fuzz > 0.0 && fuzz < 1.0 {
+            fuzz
+        } else {
+            1.0
+        };
+
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray: Ray, hit_record: HitRecord) -> Option<Scattered> {
         let reflected = reflect(ray.direction.normalize(), hit_record.normal);
-        let direction = Ray::new(hit_record.point, reflected);
+        let direction = Ray::new(hit_record.point, reflected + self.fuzz * random_unit_vector());
         let attenuation = self.albedo;
         
-        Some(Scattered { attenuation, direction })
+        if direction.direction.dot(hit_record.normal) > 0.0 {
+            Some(Scattered { attenuation, direction })
+        } else {
+            None
+        }
     }
 }
