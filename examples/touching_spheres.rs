d65ba8beb32bfd0bf9bfc5ab9729a4daec2a5bba
Sebastian Nocke <sjno@gmx.de>
2023-09-16 13:48:27 +0200

    Added example touching_spheres

---
+++ examples/touching_spheres.rs
@@ -0,0 +1,36 @@
+use std::{rc::Rc, f64::consts::PI};
+
+use glam::DVec3;
+use rust_tracer::{
+    camera::{Camera, Image},
+    hittable::Hittable,
+    material::lambertian::Lambertian,
+    sphere::Sphere,
+    world::World,
+};
+
+fn main() {
+
+    let r = (PI / 4.0).cos();
+
+    let material_left = Rc::new(Lambertian::new(DVec3::new(0.0, 0.0, 1.0)));
+    let material_right = Rc::new(Lambertian::new(DVec3::new(1.0, 0.0, 0.0)));
+
+    let objects: Vec<Box<dyn Hittable>> = vec![
+        Box::new(Sphere::new(DVec3::new(-r, 0.0, -1.0), r, material_left)),
+        Box::new(Sphere::new(DVec3::new( r, 0.0, -1.0), r, material_right)),
+    ];
+
+    let world = World::new(objects);
+
+    let position = DVec3::new(0.0, 0.0, 0.0);
+    let image = Image::from_width_aspect_ratio(400, 16.0 / 9.0, 255);
+    let mut camera = Camera::new(position, 1.0, 90.0, 100, 50, image);
+
+    camera.render_image(&world);
+
+    match camera.save_image("touching_spheres") {
+        Ok(_) => println!("Image saved successfully!"),
+        Err(_) => println!("Failed to save the image!"),
+    }
+}
